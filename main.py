4a6fe517c690ffe7e129457aae0a4f68ec1d738c
[Andrei] <[andreiaimar1362@gmail.com]>
2024-02-27 13:57:12 +0100

    Inicializando el proyecto

---
+++ main.py
@@ -0,0 +1,22 @@
+class animal:
+
+    """
+    1. ¿Que palabra reservada se utiliza en lugar de animal(nombre de la clase) al atributo patas?
+    2.¿Qué palabra reservada hay que utilizar para crear un nuevo objeto?
+
+    """
+    def caminar():
+        print("caminando", animal.patas ,"patas")
+
+
+def main():
+    vaca = animal
+    vaca.patas = 4
+    vaca.caminar
+
+    pato = animal
+    pato.patas = 2
+    pato.caminar
+
+if __name__ == "__main__":
+    main()
