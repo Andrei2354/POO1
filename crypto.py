6f562ad8b8cc83a6e7b1877082ddc1e26bd8061b
[Andrei] <[andreiaimar1362@gmail.com]>
2024-03-01 10:41:24 +0100

    Crypto

---
+++ crypto.py
@@ -0,0 +1,19 @@
+class Crypto:
+    def __init__(self, nombre: str, euros:float):
+        self.nombre = nombre
+        self.euros = euros
+        self.min = 0.0
+        self.max = 0.0
+    
+    def stopLoss(self, min:float, max:float):
+        self.min = min
+        self.max = max
+    
+    def imprimir(self):
+        print(f"El valor de {self.nombre} es {self.euros} ")
+        print(f"StopLoos. Min: {self.min} StopLoos. Max: {self.max} ")
+
+if __name__ == "__main__":
+    bitcoin = Crypto("Bitcoin", 57000)
+    bitcoin.stopLoss(55000, 60000)
+    bitcoin.imprimir()
